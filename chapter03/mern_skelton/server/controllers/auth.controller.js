718eb338d49e68e1cc9b9ab8079db6ea024840c3
s-techlead <sebastienschnd.techlead@gmail.com>
2022-06-12 18:13:12 +0200

    Add auth routes and auth controller skelton

---
+++ chapter03/mern_skelton/server/controllers/auth.controller.js
@@ -0,0 +1,22 @@
+import User from '../models/user.model'
+import jwt from 'jsonwebtoken'
+import expressJwt from 'express-jwt'
+import config from './../../config/config'
+
+const signin = (req, res) => {
+
+}
+
+const signout = (req, res) => {
+
+}
+
+const requireSignin = (req, res) => {
+
+}
+
+const hasAuthorization = (req, res) => {
+
+}
+
+export default { signin, signout, requireSignin, hasAuthorization }
